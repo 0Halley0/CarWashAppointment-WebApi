ea25bf43374a60e3def20b70dfd10d082eb3fef9
0Halley0 <hale_ino@hotmail.com>
2023-01-31 17:43:55 +0300

    Add project files.

---
+++ EntityLayer/Concrete/CarWash.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    internal class CarWash
+    {
+        [Key]
+        public int CarWashId { get; set; }
+        public int ClientNumber { get; set; }
+        public int CarWashPhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string CarWashName { get; set; }
+        public string CarWashAddress { get; set; }
+        public string CarWashCity { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CustomerImage1 { get; set; }
+        public string CustomerImage2 { get; set; }
+        public string Password { get; set; }
+        public string PlateNumber { get; set; }
+    }
+}
