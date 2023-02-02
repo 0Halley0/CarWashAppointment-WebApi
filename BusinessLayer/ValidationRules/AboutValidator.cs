434f997427d56d4201dda37b3dca4a97ba1b0984
0Halley0 <hale_ino@hotmail.com>
2023-02-02 20:03:20 +0300

    examples

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class AboutValidator : AbstractValidator<About>
+	{
+		public AboutValidator()
+		{
+			RuleFor(x => x.Details1).NotEmpty().WithMessage("Açıklama kısmını boş geçemezsiniz.");
+			RuleFor(x => x.Details1).MinimumLength(50).MaximumLength(1500).WithMessage("Karakter sayısı 50-1500 arası olmalıdır.");
+		}
+	}
+}
